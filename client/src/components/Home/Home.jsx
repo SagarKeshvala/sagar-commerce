2678337e76ec706c34e0dfc4001d6cdbb92ccedc
sagar <sagarkeshwala15@gmail.com>
2023-04-26 08:37:45 +0530

    Added Category section on home page

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,12 +1,18 @@
 import Banner from "./Banner/Banner";
+import Category from "./Category/Category";
 import "./Home.scss";
 
 const Home = () => {
-	return (
-		<div>
-			<Banner />
-		</div>
-	);
+  return (
+    <div>
+      <Banner />
+      <div className="main-content">
+        <div className="layout">
+          <Category />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
